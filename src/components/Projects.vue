02758c1537f330d84087a364d39cac24b63011e4
Howakyl <howakyle96@live.com>
2021-12-07 18:18:35 -0800

    add projects section

---
+++ src/components/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <div class="container my-work-title">
+      <h2 id="my-work" class="about-title">My Work</h2>
+      <div class="line"></div>
+    </div>
+  </section>
+</template>
+
+<script>
+import "~/assets/Projects.css";
+export default {};
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,45 +1,47 @@
 <template>
   <main>
     <Hero />
+    <Projects />
   </main>
 </template>
 
 <script>
 import Hero from "../components/Hero.vue";
+import Projects from "../components/Projects.vue";
 export default {
-  components: { Hero },
+  components: { Hero, Projects },
 };
 </script>
 
 <style>
-  :root {
-    --primary-color: #ededed;
-    --secondary-color: #2e2d2d;
-    --main-accent: #295ea4;
-    --secondary-accent: #f0a32f;
-    --text-accent: #4d4d4d;
-    --title-font: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
-      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
-  }
+:root {
+  --primary-color: #ededed;
+  --secondary-color: #2e2d2d;
+  --main-accent: #295ea4;
+  --secondary-accent: #f0a32f;
+  --text-accent: #4d4d4d;
+  --title-font: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
 
-  [data-theme="dark"] {
-    --primary-color: #1b1b1b;
-    --secondary-color: #EDEDED;
-    --main-accent: #2b62aa;
-    --secondary-accent: #F0A32F;
-    --text-accent: #c5c5c5;
+[data-theme="dark"] {
+  --primary-color: #1b1b1b;
+  --secondary-color: #ededed;
+  --main-accent: #2b62aa;
+  --secondary-accent: #f0a32f;
+  --text-accent: #c5c5c5;
 }
 
 * {
-    box-sizing: border-box;
-    margin: 0;
+  box-sizing: border-box;
+  margin: 0;
 }
 
 body {
-    background-color: var(--primary-color);
-    color: var(--secondary-color);
-    font-family: 'Open Sans' , sans-serif;
-    min-height: 100vh;
-    transition: background .3s linear;
+  background-color: var(--primary-color);
+  color: var(--secondary-color);
+  font-family: "Open Sans", sans-serif;
+  min-height: 100vh;
+  transition: background 0.3s linear;
 }
 </style>

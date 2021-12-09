2582645de916f7bd5bc5b0812d726fdba2c249a5
Howakyl <howakyle96@live.com>
2021-12-09 15:06:10 -0800

    add projects to template

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,6 +10,31 @@
       :techUsed="fishilyTech"
       :projectImage="'fishily-map.png'"
       :visitLink="'https://fishily.netlify.app/'"
+      :githubLink="'https://github.com/Howakyl/fishily-client'"
+    />
+    <Project
+      :title="'Wayfarer'"
+      :description="`Wayfarer is a travel app where users can create profiles, make posts showing off their favorite travel destinations, and comment on other users' posts. Full CRUD on Users, Posts, and Comments.`"
+      :techUsed="wayfarerTech"
+      :projectImage="'wayfarer.png'"
+      :visitLink="'http://wayfarer-kemt.herokuapp.com/'"
+      :githubLink="'https://github.com/Howakyl/wayfarer'"
+    />
+    <Project
+      :title="'myTrax'"
+      :description="'Create, share, and listen to your own custom song (and video) playlists all in one place, on myTrax! By utilzing YouTube or Spotify APIs, users can listen to, upload, create, and delete playlists.'"
+      :techUsed="myTraxTech"
+      :projectImage="'myTrax.png'"
+      :visitLink="'https://enigmatic-cliffs-18729.herokuapp.com/'"
+      :githubLink="'https://github.com/Howakyl/myTrax'"
+    />
+    <Project
+      :title="'Tamagotchi'"
+      :description="'A browser game based on the 1996 hit handheld game! Take care of your pet, by feeding, playing, and letting them rest.'"
+      :techUsed="wayfarerTech"
+      :projectImage="'Tamagotchi.png'"
+      :visitLink="'https://pages.git.generalassemb.ly/Howakyl96/project-0-tomagotchi/'"
+      :githubLink="'https://github.com/Howakyl/Tamagotchi-Life'"
     />
   </section>
 </template>
@@ -40,6 +65,28 @@ export default {
         "CSS3",
         "HTML5",
       ],
+      wayfarerTech: [
+        "Python",
+        "Django",
+        "postgreSQL",
+        "Heroku",
+        "Javascript",
+        "Materialize",
+        "CSS3",
+        "HTML5",
+      ],
+      myTraxTech: [
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "EJS",
+        "Javascript",
+        "Heroku",
+        "Bootstrap",
+        "CSS3",
+        "HTML5",
+      ],
+      tamagotchiTech: ["jQuery", "Typescript", "CSS3", "HTML5"],
     };
   },
 };

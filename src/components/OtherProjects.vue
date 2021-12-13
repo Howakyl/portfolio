3eab230df2aaa8bc669609f019661280f66f3700
Howakyl <howakyle96@live.com>
2021-12-12 20:36:40 -0800

    add sms bot to otherprojects

--- src/components/OtherProjects.vue
+++ src/components/OtherProjects.vue
@@ -9,59 +9,12 @@
         :visitLink="'https://repofinder.netlify.app/'"
         :techUsed="repoFinder"
       />
-      <!-- <div class="other-projects-folder">
-        <div class="other-projects-header">
-          <a
-            href="https://github.com/Howakyl/repo-finder"
-            target="_blank"
-            class="other-link"
-          >
-            <h4>RepoFinder</h4>
-          </a>
-          <div>
-            <a href="https://github.com/Howakyl/repo-finder" target="_blank">
-              <i class="fab fa-github"></i>
-            </a>
-            <a href="https://repofinder.netlify.app/" target="_blank">
-              <i class="fas fa-external-link-alt"></i>
-            </a>
-          </div>
-        </div>
-        <p>
-          Search for any GitHub Organization, see recent pull requests, stars,
-          and more.
-        </p>
-        <p>
-          React <span class="divider">|</span> Javascript
-          <span class="divider">|</span> CSS3
-          <span class="divider">|</span> HTML5
-        </p>
-      </div> -->
-
-      <div class="other-projects-folder">
-        <div class="d-flex other-projects-header">
-          <a
-            href="https://github.com/Howakyl/sms-bot"
-            target="_blank"
-            class="other-link"
-          >
-            <h4>SMS Bot</h4>
-          </a>
-          <div>
-            <a href="https://github.com/Howakyl/sms-bot" target="_blank">
-              <i class="fab fa-github"></i>
-            </a>
-          </div>
-        </div>
-        <p>
-          Utilize Twilio's API to text the bot and recieve some cat pictures,
-          and inspirational quotes!
-        </p>
-        <p>
-          Python <span class="divider">|</span> Flask
-          <span class="divider">|</span> Twilio
-        </p>
-      </div>
+      <OtherProject
+        :githubLink="'https://github.com/Howakyl/sms-bot'"
+        :title="'SMS Bot'"
+        :description="`Utilize Twilio's API to text the bot and recieve some cat pictures,and inspirational quotes!`"
+        :techUsed="smsBot"
+      />
     </section>
   </div>
 </template>
@@ -77,12 +30,8 @@ export default {
   },
   data() {
     return {
-      repoFinder: [
-        'React',
-        'Javascript',
-        'CSS3',
-        'HTML5'
-      ],
+      repoFinder: ["React", "Javascript", "CSS3", "HTML5"],
+      smsBot: ["Python", "Flask", "Twilio"],
     };
   },
 };

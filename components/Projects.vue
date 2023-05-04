fb96f63f3a30202893a23989cbdcd2cfac9438a4
Howakyl <howakyle96@live.com>
2023-05-04 11:27:23 -0700

    add new projects, showmore button

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,52 +1,62 @@
 <template>
-  <section>
+  <section class="projects-wrapper">
     <div class="container my-work-title">
       <h2 id="my-work" class="about-title">My Work</h2>
       <LineHeader />
     </div>
-    <Project
-      :title="'Story Tyke'"
-      :description="storyTykeDescription"
-      :techUsed="storyTykeTech"
-      :projectImage="'storyTyke2.png'"
-      :visitLink="'https://play.google.com/store/apps/details?id=com.storytyke.storytykeandroid&hl=en_US&gl=US'"
-    />
-    <Project
-      :title="'Trapdoor Creative'"
-      :description="trapdoorDescription"
-      :techUsed="trapdoorTech"
-      :projectImage="'trapdoor.png'"
-      :visitLink="'https://www.trapdoorcreative.com/'"
-    />
-    <Project
-      :title="'eFinalDate'"
-      :description="`Built with the team at XLR8, eFinalDate simplifies the monument making process. By utilizing the ''MagicCard'' to eliminate the need for messy rubbings. Just take a photo with the card, and the image is sized and aligned perfectly for your editing software! I managed the creation of the Nuxt.js frontend, and the iOS companion app.`"
-      :techUsed="eFinalDate"
-      :projectImage="'efd3.png'"
-      :visitLink="'https://alpha-efinaldate.netlify.app/'"
-    />
-    <Project
-      :title="'Cheerlending'"
-      :description="'Cheerlending is an app made to help California residents find a lower interest rate on their mortgage. I was focused on the frontend features of this project along with the team at XLR8, as well as creating and managing analytics features for our client.'"
-      :techUsed="cheerlendingTech"
-      :projectImage="'cheerlending.webP'"
-      :visitLink="'https://alpha.californiaratereduction.com/onboarding'"
-    />
-    <Project
-      :title="'Fishily'"
-      :description="'Fishily is an app made to help new fishermen find places to catch fish, and for experienced anglers to show off their catches. Users can create profiles, upload posts with optional coordinates, and leave comments.'"
-      :techUsed="fishilyTech"
-      :projectImage="'fishily-map.png'"
-      :visitLink="'https://fishily.netlify.app/'"
-      :githubLink="'https://github.com/Howakyl/fishily-client'"
-    />
-    <Project
-      :title="'Wayfarer'"
-      :description="`Wayfarer is a travel app where users can create profiles, make posts showing off their favorite travel destinations, and comment on other users' posts. Full CRUD on Users, Posts, and Comments.`"
-      :techUsed="wayfarerTech"
-      :projectImage="'wayfarer.png'"
-      :visitLink="'http://wayfarer-kemt.herokuapp.com/'"
-      :githubLink="'https://github.com/Howakyl/wayfarer'"
+    <div class="projects-container">
+      <Project
+        :title="'Story Tyke'"
+        :description="storyTykeDescription"
+        :techUsed="storyTykeTech"
+        :projectImage="'storyTyke2.png'"
+        :visitLink="'https://play.google.com/store/apps/details?id=com.storytyke.storytykeandroid&hl=en_US&gl=US'"
+      />
+      <Project
+        :title="'Trapdoor Creative'"
+        :description="trapdoorDescription"
+        :techUsed="trapdoorTech"
+        :projectImage="'trapdoor.png'"
+        :visitLink="'https://www.trapdoorcreative.com/'"
+      />
+      <Project
+        :title="'eFinalDate'"
+        :description="`Built with the team at XLR8, eFinalDate simplifies the monument making process. By utilizing the ''MagicCard'' to eliminate the need for messy rubbings. Just take a photo with the card, and the image is sized and aligned perfectly for your editing software! I managed the creation of the Nuxt.js frontend, and the iOS companion app.`"
+        :techUsed="eFinalDate"
+        :projectImage="'efd3.png'"
+        :visitLink="'https://alpha-efinaldate.netlify.app/'"
+      />
+      <Project
+        :title="'Cheerlending'"
+        :description="'Cheerlending is an app made to help California residents find a lower interest rate on their mortgage. I was focused on the frontend features of this project along with the team at XLR8, as well as creating and managing analytics features for our client.'"
+        :techUsed="cheerlendingTech"
+        :projectImage="'cheerlending.webP'"
+        :visitLink="'https://alpha.californiaratereduction.com/onboarding'"
+      />
+      <Project
+        v-if="showMore"
+        :title="'Fishily'"
+        :description="'Fishily is a personal project made to help new fishermen find places to catch fish, and for experienced anglers to show off their catches. Users can create profiles, upload posts with optional coordinates, and leave comments.'"
+        :techUsed="fishilyTech"
+        :projectImage="'fishily-map.png'"
+        :visitLink="'https://fishily.netlify.app/'"
+        :githubLink="'https://github.com/Howakyl/fishily-client'"
+      />
+      <Project
+        v-if="showMore"
+        :title="'Wayfarer'"
+        :description="`Built with my classmates at General Assembly, Wayfarer is a travel app where users can create profiles, make posts showing off their favorite travel destinations, and comment on other users' posts. Full CRUD on Users, Posts, and Comments.`"
+        :techUsed="wayfarerTech"
+        :projectImage="'wayfarer.png'"
+        :visitLink="'http://wayfarer-kemt.herokuapp.com/'"
+        :githubLink="'https://github.com/Howakyl/wayfarer'"
+      />
+    </div>
+
+    <Button
+      class="site-link showmore-btn"
+      :content="showMore ? 'Show Less' : 'Show More'"
+      :onClick="toggleShowMore"
     />
   </section>
 </template>
@@ -54,19 +64,20 @@
 <script>
 // TODO - PROJECTS TO ADD:
 // Pixingo
-// Trapdoor Creative
 // Tiered System Support
-// Story Tyke
 import "~/assets/Projects.css";
 import LineHeader from "./LineHeader.vue";
 import Project from "./Project.vue";
+import Button from "./Button.vue";
 export default {
   components: {
     LineHeader,
     Project,
+    Button,
   },
   data() {
     return {
+      showMore: false,
       cheerlendingTech: [
         "React",
         "Django",
@@ -106,10 +117,16 @@ export default {
       storyTykeTech: ["Java", "Firebase", "Django", "Python"],
       trapdoorTech: ["React", "Node.js", "AWS", "Firebase"],
       trapdoorDescription:
-        "In collaboration with the XLR8 Team and Trapdoor Creative, I created an in-browser file explorer/uploader for users 3D assets, as well as overhauling Trapdoor's account management pages.",
+        "In collaboration with the XLR8 Team and Trapdoor Creative, I created an in-browser file explorer/uploader for user's 3D assets, as well as overhauling Trapdoor Creative's account management page.",
       storyTykeDescription: `The Story Tyke android app provides bedtime stories for your children in text, picture, and audio formats! 
-      With the XLR8 team I oversaw project planning, creation of UI elements, payment integration, and API implementation.`,
+      With the XLR8 team I oversaw project planning, creation of UI elements, audiobook features, payment integration, and API implementation.`,
     };
+  },
+  methods: {
+    toggleShowMore() {
+      this.showMore = !this.showMore;
+      console.log(this.showMore);
+    },
   },
 };
 </script>
